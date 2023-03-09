62b0f8c161f484e67787e21146da3dfe44171d22
Charles Zhang <charleszhangmb@gmail.com>
2023-03-08 18:14:54 -0800

    Navbar and theming

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+// Stylesheet
+import styles from "./Navbar.module.scss";
+// Next
+import Link from "next/link";
+
+const Navbar: React.FC = () => {
+    return (
+        <div className={styles.navbar}>
+            <div className={styles["navbar-container"]}>
+                <Link href="/">
+                    <a>
+                        <h1 className={styles.name}>Charles Zhang</h1>
+                    </a>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,45 @@
 import "@/styles/globals.scss";
 // TS
 import type { AppProps } from "next/app";
+// React
+import { useEffect } from "react";
+// Nav components
+import Navbar from "@/components/Navbar/Navbar";
 
 function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    // Check user preferences for theme
+    const getTheme = () => {
+        if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+            return "light";
+        } else {
+            return "dark";
+        }
+    };
+
+    // Set site theme to given theme
+    const setTheme = (theme: string) => {
+        document.body.dataset.theme = theme;
+    };
+
+    // Set event listener for dynamic updates
+    useEffect(() => {
+        const updateTheme = () => {
+            setTheme(getTheme());
+        };
+        let themeQuery = window.matchMedia("(prefers-color-scheme: light)");
+        themeQuery.addEventListener("change", updateTheme);
+        updateTheme();
+        return () => {
+            document.removeEventListener("change", updateTheme);
+        };
+    }, []);
+
+    return (
+        <div id="app">
+            <Navbar />
+            <Component {...pageProps} />
+        </div>
+    );
 }
 
 export default App;
