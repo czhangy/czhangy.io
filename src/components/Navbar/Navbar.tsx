122164b7007e6db7a6224dd1dc9173be75243330
Charles Zhang <charleszhangmb@gmail.com>
2023-03-08 20:42:44 -0800

    Temporary color scheme

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,15 +5,18 @@ import Link from "next/link";
 
 const Navbar: React.FC = () => {
     return (
-        <div className={styles.navbar}>
-            <div className={styles["navbar-container"]}>
-                <Link href="/">
-                    <a>
-                        <h1 className={styles.name}>Charles Zhang</h1>
-                    </a>
-                </Link>
+        <>
+            <div className={styles.navbar}>
+                <div className={styles["navbar-container"]}>
+                    <Link href="/">
+                        <a>
+                            <h1 className={styles.name}>Charles Zhang</h1>
+                        </a>
+                    </Link>
+                </div>
             </div>
-        </div>
+            <div className={`${styles["navbar-bg"]} bg`} />
+        </>
     );
 };
 
