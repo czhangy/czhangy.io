5a1061cf7571749e5360519156d93eb6ad886e79
Charles Zhang <charleszhangmb@gmail.com>
2023-03-12 14:34:08 -0700

    Layout skeleton

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -2,7 +2,19 @@
 import styles from "./HomePage.module.scss";
 
 const HomePage: React.FC = () => {
-    return <div className={styles["home-page"]}>HomePage</div>;
+    return (
+        <div className={styles["home-page"]}>
+            <div className={styles["home-grid"]}>
+                <div className={`${styles["nav-card"]} ${styles.about}`}></div>
+                <div className={styles["nav-card"]}></div>
+                <div className={styles["nav-card"]}></div>
+                <div className={styles["social-card"]}></div>
+                <div className={styles["social-card"]}></div>
+                <div className={styles["social-card"]}></div>
+                <div className={styles["social-card"]}></div>
+            </div>
+        </div>
+    );
 };
 
 export default HomePage;
