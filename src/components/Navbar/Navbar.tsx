ff357ecee8f0cad00593b47ecc9ea6fdd850ba11
Charles Zhang <charleszhangmb@gmail.com>
2023-03-14 16:42:51 -0700

    Page nav added to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,11 +7,40 @@ const Navbar: React.FC = () => {
     return (
         <header className={styles.navbar}>
             <div className={styles["navbar-container"]}>
-                <Link href="/">
-                    <a>
-                        <h1 className={styles.name}>Charles Zhang</h1>
-                    </a>
-                </Link>
+                <div className={styles["main-nav"]}>
+                    <Link href="/">
+                        <a>
+                            <h1 className={styles.name}>Charles Zhang</h1>
+                        </a>
+                    </Link>
+                </div>
+                <div className={styles["page-nav"]}>
+                    <Link href="/">
+                        <a className={styles["home-link"]}>
+                            <h2 className={styles["nav-link"]}>Home</h2>
+                        </a>
+                    </Link>
+                    <span
+                        className={`${styles["v-bar"]} ${styles["home-v-bar"]}`}
+                    />
+                    <Link href="/about">
+                        <a>
+                            <h2 className={styles["nav-link"]}>About</h2>
+                        </a>
+                    </Link>
+                    <span className={styles["v-bar"]} />
+                    <Link href="/projects">
+                        <a>
+                            <h2 className={styles["nav-link"]}>Projects</h2>
+                        </a>
+                    </Link>
+                    <span className={styles["v-bar"]} />
+                    <Link href="/experience">
+                        <a>
+                            <h2 className={styles["nav-link"]}>Experience</h2>
+                        </a>
+                    </Link>
+                </div>
             </div>
         </header>
     );
