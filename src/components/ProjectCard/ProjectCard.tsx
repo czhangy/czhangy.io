fda335995ffa9c57db3dd51951217637c1dde101
Charles Zhang <charleszhangmb@gmail.com>
2023-03-17 23:10:01 -0700

    Project cards done

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,5 +1,7 @@
 // Stylesheet
 import styles from "./ProjectCard.module.scss";
+// Next
+import Image from "next/image";
 // TS
 import Project from "@/models/Project";
 // React
@@ -26,6 +28,58 @@ const ProjectCard: React.FC<Props> = ({ project }: Props) => {
             }`}
         >
             <h3 className={styles["project-name"]}>{project.name}</h3>
+            <div className={styles["project-info"]}>
+                <div className={styles["project-text"]}>
+                    <p className={styles["project-category"]}>
+                        {project.category}
+                    </p>
+                    <p className={styles["project-timeline"]}>
+                        {project.startDate} – {project.endDate}
+                    </p>
+                </div>
+                <div className={styles["project-links"]}>
+                    {project.git ? (
+                        <a
+                            href={project.git}
+                            target="_blank"
+                            className={styles["icon-container"]}
+                        >
+                            <Image
+                                src="/assets/icons/git.svg"
+                                alt="Git Repo"
+                                layout="fill"
+                                objectFit="contain"
+                            />
+                        </a>
+                    ) : (
+                        ""
+                    )}
+                    {project.link ? (
+                        <a
+                            href={project.link}
+                            target="_blank"
+                            className={styles["icon-container"]}
+                        >
+                            <Image
+                                src="/assets/icons/link.svg"
+                                alt="Site Link"
+                                layout="fill"
+                                objectFit="contain"
+                            />
+                        </a>
+                    ) : (
+                        ""
+                    )}
+                </div>
+            </div>
+            <div className={styles.arrow}>
+                <Image
+                    src="/assets/icons/chevron.svg"
+                    alt="Arrow indicator"
+                    layout="fill"
+                    objectFit="contain"
+                />
+            </div>
         </button>
     );
 };
--- src/models/Project.ts
+++ src/models/Project.ts
@@ -2,6 +2,8 @@ export default interface Project {
     name: string;
     slug: string;
     category: string;
+    startDate: string;
+    endDate: string;
     link: string | null;
     git: string | null;
 }
