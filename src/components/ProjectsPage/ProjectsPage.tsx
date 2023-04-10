c27455392d0bdd9cc4ae262e027098926bfc1560
Charles Zhang <charleszhangmb@gmail.com>
2023-04-10 14:49:14 -0700

    Project images added

--- src/components/ProjectsPage/ProjectsPage.tsx
+++ src/components/ProjectsPage/ProjectsPage.tsx
@@ -9,13 +9,13 @@ import ProjectsMenu from "@/components/ProjectsMenu/ProjectsMenu";
 
 const ProjectsPage: React.FC = () => {
     // Menu state
-    const [currentProject, setCurrentProject] = useState<string>("");
+    const [currentProject, setCurrentProject] = useState<string>("default");
 
     return (
         <div className={styles["projects-page"]}>
             <div className={styles["project-img"]}>
                 <Image
-                    src={`/assets/images/projects/default.jpg`}
+                    src={`/assets/images/projects/${currentProject}.webp`}
                     alt=""
                     layout="fill"
                     objectFit="cover"
