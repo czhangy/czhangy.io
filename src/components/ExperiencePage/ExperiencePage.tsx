fcbbe7cf854de5e10c63adcd3284a84b9e5d3f12
Charles Zhang <charleszhangmb@gmail.com>
2023-04-13 17:19:23 -0700

    Timeline done

--- src/components/ExperiencePage/ExperiencePage.tsx
+++ src/components/ExperiencePage/ExperiencePage.tsx
@@ -11,13 +11,34 @@ const ExperiencePage: React.FC = () => {
     return (
         <div className={styles["experience-page"]}>
             <ul className={styles.timeline}>
+                <div className={styles.arrow} />
                 {experience.map((e: Experience, i: number) => {
                     return (
-                        <li className={styles["timeline-el"]} key={i}>
+                        <li
+                            className={`${styles["timeline-el"]} ${
+                                i % 2 === 0 ? styles.right : styles.left
+                            }`}
+                        >
                             <ExperienceCard experience={e} />
+                            <p
+                                className={`${styles.time} ${
+                                    i % 2 === 0
+                                        ? styles["right-time"]
+                                        : styles["left-time"]
+                                }`}
+                            >{`${e.startDate} ${
+                                e.endDate.length > 0 ? `- ${e.endDate}` : ""
+                            }`}</p>
                         </li>
                     );
                 })}
+                <div
+                    className={`${styles.endpoint} ${
+                        experience.length % 2 === 0
+                            ? styles["right-endpoint"]
+                            : styles["left-endpoint"]
+                    }`}
+                />
             </ul>
         </div>
     );
