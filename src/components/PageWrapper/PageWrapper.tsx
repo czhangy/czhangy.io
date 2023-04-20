35c70d933454874d88755cc5d25af6a47e4a38a8
Charles Zhang <charleszhangmb@gmail.com>
2023-04-19 19:50:05 -0700

    Fix page load animations

--- src/components/PageWrapper/PageWrapper.tsx
+++ src/components/PageWrapper/PageWrapper.tsx
@@ -2,28 +2,14 @@
 import styles from "./PageWrapper.module.scss";
 // TS
 import { ReactNode } from "react";
-// React
-import { useEffect, useState } from "react";
 
 type Props = {
     children: ReactNode;
 };
 
 const PageWrapper: React.FC<Props> = ({ children }) => {
-    // Wrapper state
-    const [fade, setFade] = useState<boolean>(true);
-
-    // Trigger fade on load
-    useEffect(() => {
-        setFade(false);
-    }, []);
-
     return (
-        <div
-            className={`${styles["page-wrapper"]} ${
-                fade ? styles.hide : styles.show
-            }`}
-        >
+        <div className={styles["page-wrapper"]}>
             <div className={styles["page-container"]}>{children}</div>
         </div>
     );
