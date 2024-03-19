7678ea926c533e777820618bee2620b7ec97ad34
Charles Zhang <charleszhangmb@gmail.com>
2024-03-18 23:32:43 -0700

    Ignore /Test directory in coverage

--- jest.config.ts
+++ jest.config.ts
@@ -22,7 +22,11 @@ const config: Config = {
     collectCoverage: true,
 
     // An array of glob patterns indicating a set of files for which coverage information should be collected
-    collectCoverageFrom: ["src/components/**/*.tsx", "!src/mocks/*.tsx"],
+    collectCoverageFrom: [
+        "src/components/**/*.tsx",
+        "!src/mocks/*.tsx",
+        "!src/components/Test/*.tsx",
+    ],
 
     // The directory where Jest should output its coverage files
     coverageDirectory: "coverage",
