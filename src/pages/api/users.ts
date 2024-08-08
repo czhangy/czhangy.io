6825c50c0fde0ef6ed339384f4825cc98008497d
Charles Zhang <charleszhangmb@gmail.com>
2024-08-07 21:53:23 -0700

    Use new pattern in users API

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,21 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import bcrypt from "bcrypt";
-import { AdminUser } from "@prisma/client";
 
 import prisma from "@/lib/prisma";
 import {
     CONFLICT,
     CREATED,
     FAILED_REGISTER_MSG,
-    GENERIC_FAILED_MSG,
-    GET,
     INTERNAL_SERVER_ERROR,
     INVALID_HTTP_METHOD_MSG,
     METHOD_NOT_ALLOWED,
     POST,
-    UNIMPLEMENTED,
-    UNIMPLEMENTED_MSG,
-    USERNAME_ALREADY_EXISTS_MSG,
+    PRISMA_DUPLICATE,
+    TITLE_ALREADY_EXISTS_MSG,
 } from "@/static/constants";
 
 /** The number of salt rounds to use for bcrypt */
@@ -37,17 +33,6 @@ const handleRegisterRequest = async (
         const username: string = req.body.username;
         const password: string = req.body.password;
 
-        // Check if username exists and error if it does
-        const existingUser: AdminUser | null =
-            await prisma.adminUser.findUnique({
-                where: { username: username },
-            });
-        if (existingUser) {
-            return res
-                .status(CONFLICT)
-                .json({ error: USERNAME_ALREADY_EXISTS_MSG });
-        }
-
         // Create user
         await prisma.adminUser.create({
             data: {
@@ -58,10 +43,17 @@ const handleRegisterRequest = async (
 
         // Send back success
         return res.status(CREATED).json({});
-    } catch (_: any) {
-        return res
-            .status(INTERNAL_SERVER_ERROR)
-            .json({ error: FAILED_REGISTER_MSG });
+    } catch (err: any) {
+        // Check for unique constraint failure on write
+        if (err.code === PRISMA_DUPLICATE) {
+            return res
+                .status(CONFLICT)
+                .json({ message: TITLE_ALREADY_EXISTS_MSG });
+        } else {
+            return res
+                .status(INTERNAL_SERVER_ERROR)
+                .json({ message: FAILED_REGISTER_MSG });
+        }
     }
 };
 
@@ -69,19 +61,11 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse,
 ): Promise<void> {
-    try {
-        if (req.method === GET) {
-            return res.status(UNIMPLEMENTED).json({ error: UNIMPLEMENTED_MSG });
-        } else if (req.method === POST) {
-            return await handleRegisterRequest(req, res);
-        } else {
-            return res.status(METHOD_NOT_ALLOWED).json({
-                error: INVALID_HTTP_METHOD_MSG,
-            });
-        }
-    } catch (_: any) {
-        return res
-            .status(INTERNAL_SERVER_ERROR)
-            .json({ error: GENERIC_FAILED_MSG });
+    if (req.method === POST) {
+        return await handleRegisterRequest(req, res);
+    } else {
+        return res.status(METHOD_NOT_ALLOWED).json({
+            error: INVALID_HTTP_METHOD_MSG,
+        });
     }
 }
